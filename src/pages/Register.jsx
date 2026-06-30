7c9544397c71b16053a69d05f937eb7b36e34e4f
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-06-30 14:53:38 +0700

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -79,7 +79,7 @@ function Register() {
               required
               style={{ textTransform: "uppercase" }}
               className="w-full px-4 py-2.5 rounded-lg border border-gray-300 text-gray-900 text-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition"
-              placeholder="Contoh: ABC"
+              placeholder="Initial"
             />
           </div>
 
