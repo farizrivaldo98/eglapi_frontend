53cced976296ea33e95ab44960a26af8dd241c5f
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-07-01 11:40:28 +0700

    Update Scadamonitor.jsx

--- src/pages/Scadamonitor.jsx
+++ src/pages/Scadamonitor.jsx
@@ -244,7 +244,7 @@ export default function Scadamonitor() {
             <Button 
               onClick={handleSaveToPlc} 
               colorScheme="blue"
-              isDisabled={userGlobal?.level < 5 || saving}
+              isDisabled={userGlobal.level > 4}
               isLoading={saving}
               loadingText="Saving..."
             >
