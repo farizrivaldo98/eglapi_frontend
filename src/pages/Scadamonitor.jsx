dd97b5351741c1635da93806baed19f0f665a990
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-07-01 13:08:38 +0700

    Update Scadamonitor.jsx

--- src/pages/Scadamonitor.jsx
+++ src/pages/Scadamonitor.jsx
@@ -166,7 +166,7 @@ export default function Scadamonitor() {
       <div className="p-4 flex gap-4 items-center">
         {userGlobal.level == null ? (
           <>
-            <Button onClick={connectWS} size="sm" colorScheme="red">
+            <Button onClick={connectWS} size="sm" href="/" colorScheme="red">
               Back
             </Button>
           </>
