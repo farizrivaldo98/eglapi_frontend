1d70c2ddc9bc02d1fc07e6c921f70b48f6fb7c00
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-08-04 09:29:17 +0700

    update Energy

---
+++ src/pages/Energy.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import EnergyPower from "./EnergyPower"
+import EnergyWater from "./EnergyWater"
+
+
+const API_BASE = "http://10.163.0.66:8002/part";
+
+
+
+function Energy() {
+  const [activeTab, setActiveTab] = useState(0); 
+  const [isDarkMode, setIsDarkMode] = useState(
+    document.documentElement.getAttribute("data-theme") === "dark"
+  );
+     useEffect(() => {
+    const handleThemeChange = () => {
+      const currentTheme = document.documentElement.getAttribute("data-theme");
+      setIsDarkMode(currentTheme === "dark");
+    };
+    const observer = new MutationObserver(handleThemeChange);
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["data-theme"],
+    });
+    return () => observer.disconnect();
+  }, []);
+
+
+  return (
+   <div>
+      <Tabs
+        index={activeTab}
+        onChange={setActiveTab}
+        isFitted
+        variant="enclosed"
+        colorScheme="blue"
+        className="mx-4 md:mx-20 mt-4"
+      >
+        <TabList>
+          <Tab>Power Meter</Tab>
+          <Tab>Water Meter</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel px={0}>
+            <EnergyPower channels={CHANNELS} metrics={METRICS} apiBase={API_BASE} isDarkMode={isDarkMode} />
+          </TabPanel>
+          <TabPanel px={0}>
+            <EnergyWater channels={CHANNELS} metrics={METRICS} apiBase={API_BASE} isDarkMode={isDarkMode} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  )
+}
+
+export default Energy
---
+++ src/pages/EnergyPower.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EnergyPower() {
+  return (
+    <div>EnergyPower</div>
+  )
+}
+
+export default EnergyPower
