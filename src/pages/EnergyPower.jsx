765cd71ae046d2a8c5f58d74cff818abec1c4161
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-08-08 11:39:56 +0700

    Update EnergyPower.jsx

--- src/pages/EnergyPower.jsx
+++ src/pages/EnergyPower.jsx
@@ -19,9 +19,9 @@ function EnergyPower() {
     <div>
       <Tabs variant="enclosed" colorScheme="blue" isFitted>
         <TabList className="w-full lg:w-3/4" display="flex">
-          <Tab>Total Energy</Tab>
+          {/* <Tab>Total Energy</Tab> */}
           <Tab>Analisa Parameter Listrik</Tab>
-          <Tab>Perbandingan Meter</Tab>
+          <Tab>Total Enegy Meter</Tab>
         </TabList>
         <TabPanels>
           <TabPanel px={0}>
